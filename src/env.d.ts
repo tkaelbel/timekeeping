2c8951c3f127ecc75892d8d274c77507cb1291fc
Tim K <tkaelbel@web.de>
2022-06-08 21:41:18 +0200

    Added env files for running mode. Adjusted scripts and added web, to run the app in the browser

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,17 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_MODE: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
